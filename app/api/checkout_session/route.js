0f0fdb2c178a75da06b8ff1cff128b2e8b89bda5
Shayan Hussain <shayan.hussainzardari@gmail.com>
2024-08-20 18:07:17 -0400

    srtipe setup complete

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -1,3 +1,4 @@
+import { connectFirestoreEmulator } from 'firebase/firestore'
 import {NextResponse} from 'next/server'
 import Stripe from 'stripe'
 
@@ -5,6 +6,20 @@ import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const formatAmountForStripe = (amount) => {
      return Math.round(amount * 100)
+}
+
+export async function GET(req) {
+  const searchParams = req.nextURL.searchParams  
+  const session_id = searchParams.get('session_id')
+
+  try {
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+    return NextResponse.json(checkoutSession)
+  } catch (error) {
+    console.error('Error retrieving checkout session: ', error)
+    return NextResponse.json({error: {message: error.message}}, {status: 500} )
+  }
+
 }
 
 export async function POST(req)  {
